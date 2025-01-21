849472960900eced32940a3fe2bae184528f9e55
sabrinasanmi <sanmisabrina@icloud.com>
2025-01-21 02:16:39 -0300

    Config. Swagger

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -27,7 +28,8 @@ public class Usuario {
 	
 	@NotNull(message = "O Atributo nome é Obrigatótio")
 	private String nome;
-
+	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O Atributo usuario é Obrigatótio")
 	@Email(message = "O Atributo usuario deve ser um email válido!")
 	private String usuario;
